478a07aee2412339fa4169a32e3d6ef3b830ce4b
zqloner <526336589@qq.com>
2019-11-16 14:24:22 +0800

    fastdfs上传

---
+++ fastdfs-upload/src/main/java/com/zq/service/impl/HouseInfoServiceImpl.java
@@ -0,0 +1,33 @@
+package com.zq.service.impl;
+
+import com.zq.mapper.HouseInfoMapper;
+import com.zq.pojo.HouseInfo;
+import com.zq.service.HouseInfoService;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+/**
+ * @Description:
+ * @Company: 千锋互联
+ * @Author: 李丽婷
+ * @Date: 2019/7/22
+ * @Time: 下午4:35
+ */
+@Service
+public class HouseInfoServiceImpl implements HouseInfoService {
+
+    @Resource
+    private HouseInfoMapper houseInfoMapper;
+
+    @Override
+    public boolean saveHouseInfo(HouseInfo houseInfo) {
+        return houseInfoMapper.saveHouseInfo(houseInfo)>0;
+    }
+
+    @Override
+    public List<HouseInfo> findAll() {
+        return houseInfoMapper.findAll();
+    }
+}
